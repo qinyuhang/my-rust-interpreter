1193ac8a7e9c4e1f0a367513e6fd88a052d9b96d
qinyuhang <qinyuhangxiaoxiang@gmail.com>
2023-01-30 15:16:38 +0800

    feat: eval int infix expression

--- src/evaluator/mod.rs
+++ src/evaluator/mod.rs
@@ -44,7 +44,41 @@ pub fn eval(node: &dyn Node) -> Option<Rc<dyn Object>> {
             return eval_prefix_expression(&n.operator, right);
         }
     }
+    if n.is::<InfixExpression>() {
+        if let Some(n) = n.downcast_ref::<InfixExpression>() {
+            let left = eval(n.left.as_ref().unwrap().upcast());
+            let right = eval(n.right.as_ref().unwrap().upcast());
+            return eval_infix_expression(&n.operator, left, right);
+        }
+    }
     None
+}
+
+pub fn eval_infix_expression(
+    operator: &str,
+    left: Option<Rc<dyn Object>>,
+    right: Option<Rc<dyn Object>>,
+) -> Option<Rc<dyn Object>> {
+    match (left.as_ref(), right.as_ref()) {
+        (Some(l), Some(r))
+            if (left.as_ref().unwrap().as_any()).is::<Integer>()
+                && (right.as_ref().unwrap().as_any()).is::<Integer>() =>
+        {
+            let l = l.as_any().downcast_ref::<Integer>().unwrap();
+            let r = r.as_any().downcast_ref::<Integer>().unwrap();
+            if let Some(val) = match operator {
+                "+" => Some(l.value + r.value),
+                "-" => Some(l.value - r.value),
+                "*" => Some(l.value * r.value),
+                "/" => Some(l.value / r.value),
+                _ => None,
+            } {
+                return Some(Rc::new(Integer { value: val }));
+            }
+            None
+        }
+        _ => None,
+    }
 }
 
 pub fn eval_prefix_expression(
@@ -53,7 +87,8 @@ pub fn eval_prefix_expression(
 ) -> Option<Rc<dyn Object>> {
     match operator {
         "!" => eval_bang_operator_expression(right),
-        _ => None,
+        "-" => eval_minus_prefix_operator_expression(right),
+        _ => Some(Rc::new(Null {})),
     }
 }
 
@@ -71,6 +106,19 @@ pub fn eval_bang_operator_expression(right: Option<Rc<dyn Object>>) -> Option<Rc
         return Some(Rc::new(Boolean { value: false }));
     }
     Some(Rc::new(Boolean { value: false }))
+}
+
+pub fn eval_minus_prefix_operator_expression(
+    right: Option<Rc<dyn Object>>,
+) -> Option<Rc<dyn Object>> {
+    if let Some(right) = right {
+        if right.object_type() == INTEGER_OBJECT {
+            return Some(Rc::new(Integer {
+                value: -Integer::try_from(right).unwrap().value,
+            }));
+        }
+    }
+    Some(Rc::new(Null {}))
 }
 
 pub fn eval_statements(stmts: Vec<Rc<dyn Statement>>) -> Option<Rc<dyn Object>> {
