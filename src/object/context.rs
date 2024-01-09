eafc39488a8b951555ed06b8b93d50e568b7f70b
qinyuhang <qinyuhangxiaoxiang@gmail.com>
2024-01-09 17:38:15 +0800

    fix: context extend method

--- src/object/context.rs
+++ src/object/context.rs
@@ -4,27 +4,41 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
+#[derive(Debug)]
 pub struct Context {
+    pub parent: Option<Rc<Context>>,
     pub scope: RefCell<HashMap<Identifier, Rc<dyn Object>>>,
 }
 
 impl Context {
+    pub fn default() -> Self {
+        Context {
+            scope: RefCell::new(HashMap::new()),
+            parent: None,
+        }
+    }
     pub fn new() -> Self {
         Context {
             scope: RefCell::new(HashMap::new()),
+            parent: None,
         }
     }
     pub fn set(&self, name: Identifier, val: Rc<dyn Object>) {
         self.scope.borrow_mut().insert(name, val);
     }
     pub fn get(&self, name: &Identifier) -> Option<Rc<dyn Object>> {
-        self.scope.borrow().get(name).cloned()
+        if let Some(current) = self.scope.borrow().get(name).cloned() {
+            return Some(current);
+        }
+        if let Some(ref parent) = self.parent {
+            return parent.get(name);
+        }
+        None
     }
-    pub fn extend(parent: &Self) -> Self {
-        let x = parent.scope.borrow().clone();
-
+    pub fn extend(parent: Rc<Self>) -> Self {
         Context {
-            scope: RefCell::new(x),
+            scope: RefCell::new(HashMap::new()),
+            parent: Some(parent.clone()),
         }
     }
 }
@@ -63,9 +77,10 @@ mod test {
         let r = context.get(&key);
         assert!(r.is_some());
     }
+
     #[test]
     fn test_extend() {
-        let context = Context::new();
+        let context = Rc::new(Context::new());
         assert_eq!(context.scope.borrow().len(), 0);
         let key = genId("foobar");
         context.set(
@@ -78,8 +93,11 @@ mod test {
         let r = context.get(&key);
         assert!(r.is_some());
 
-        let context1 = Context::extend(&context);
-        assert_eq!(context1.scope.borrow().len(), 1);
+        let context1 = Context::extend(context.clone());
+
+        assert_eq!(context1.scope.borrow().len(), 0);
+        assert!(context1.get(&key.clone()).is_some());
+
         let c1 = genId("c1");
         context1.set(
             c1,
@@ -87,17 +105,18 @@ mod test {
                 message: "for_context1".into(),
             }),
         );
-        assert_eq!(context1.scope.borrow().len(), 2);
+        assert_eq!(context1.scope.borrow().len(), 1);
         assert_eq!(context.scope.borrow().len(), 1);
 
         let c = genId("c");
         context.set(
-            c,
+            c.clone(),
             Rc::new(ErrorObject {
                 message: "for_context".into(),
             }),
         );
-        assert_eq!(context1.scope.borrow().len(), 2);
+        assert_eq!(context1.scope.borrow().len(), 1);
         assert_eq!(context.scope.borrow().len(), 2);
+        assert!(context1.get(&c.clone()).is_some());
     }
 }
