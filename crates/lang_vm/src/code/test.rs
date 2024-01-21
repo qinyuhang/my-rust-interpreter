40e4e8b55073a8f1c5dbfb85392db549c5dfa7c4
秦宇航 <qinyuhangxiaoxiang@gmail.com>
2024-01-21 23:03:12 +0800

    feat: initial compiler & vm

---
+++ crates/lang_vm/src/code/test.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn lang_vm_canary_test() {
+        assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_make() {
+        let cases =vec![
+
+        ];
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// TODO run will run args file
+// without run is repl
+// eg:
+// `mki run -i <file>.mok` will run <file>.mok
+// `mki` will start repl
+// `mik compile -i <file>.mok -o <out_file>.mokb` will compile <file>.mok to <out_file>.mokb
+
+use clap::*;
 pub use my_rust_interpreter::repl;
 
 fn main() {
